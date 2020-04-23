cbd222c2452b221e3d3c9bc64173536c6cc0a549
Felix Paul Niemeyer <niemeyer.felix@gmail.com>
2020-04-23 22:51:42 +0200

    adding bash script to automatically generate the conductor config for running multiple agents

---
+++ generate_conductor_config.sh
@@ -0,0 +1,100 @@
+DNA_HASH=`hc hash -p ./dist/trait_implementations.dna.json | grep -Po '(?<=DNA Hash: ).*'`
+OF=generated_conductor_config.toml
+AGENT_COUNT=3
+
+echo "using dna hash $DNA_HASH"
+
+agents=""
+instances=""
+interfaces=""
+
+set i=0
+for f in /home/felixpn/.config/holochain/keys/*
+do
+	i=$((i+1))
+	key=$(basename $f)
+	echo "using agent key $key"
+	agent_id="test_agent$i"
+	agents="
+$agents 
+[[agents]]
+id = '$agent_id'
+keystore_file = '/home/felixpn/.config/holochain/keys/$key'
+name = 'testAgent$i'
+public_address = '$key'
+test_agent = true
+
+	"
+	instance_id="test-instance$i"
+	instances="
+$instances
+[[instances]]
+agent = '$agent_id'
+dna = 'hc-run-dna'
+id = '$instance_id'
+
+[instances.storage]
+type = 'memory'
+
+	"
+
+	interfaces="
+$interfaces
+[[interfaces.instances]]
+id = '$instance_id'
+
+	"
+
+	if [[ $i -eq $AGENT_COUNT ]] ;
+	then 
+		break
+	fi
+done
+
+if [[ $i -lt $AGENT_COUNT ]]
+then 
+	echo "not enough agent key files in /home/felixpn/.config/holochain/keys/" 1>&2
+	echo "generate new keys with 'hc keygen'"
+	exit 1
+fi
+
+
+echo "
+bridges = []
+persistence_dir = ''
+ui_bundles = []
+ui_interfaces = []
+
+$agents 
+
+[[dnas]]
+file = '/data/development/junto/holochain-traits/trait_implementations/dist/trait_implementations.dna.json'
+hash = '$DNA_HASH'
+id = 'hc-run-dna'
+
+$instances
+
+[[interfaces]]
+admin = true
+id = 'websocket-interface'
+
+$interfaces
+
+[interfaces.driver]
+port = 8888
+type = 'websocket'
+
+[logger]
+state_dump = true
+type = 'debug'
+
+[logger.rules]
+rules = []
+
+[passphrase_service]
+type = 'cmd'
+
+[signals]
+consistency = false
+trace = false
+" > $OF
--- zomes/social_graph/code/src/lib.rs
+++ zomes/social_graph/code/src/lib.rs
@@ -8,26 +8,26 @@ extern crate serde_json;
 extern crate holochain_json_derive;
 
 use hdk::{
-    entry_definition::ValidatingEntryType,
+    // entry_definition::ValidatingEntryType,
     error::ZomeApiResult,
 };
-use hdk::error::ZomeApiError;
+// use hdk::error::ZomeApiError;
 use hdk::holochain_core_types::{
     entry::Entry,
     dna::entry_types::Sharing,
 };
 
-use hdk::holochain_persistence_api::{
-    cas::content::Address,
-};
+// use hdk::holochain_persistence_api::cas::content::Address;
 
 use hdk::holochain_json_api::{
     error::JsonError,
     json::JsonString,
 };
 
+use hdk::holochain_persistence_api::hash::HashString;
+
 /* 	trait from here: https://github.com/juntofoundation/Holochain-Trait-Definitions
-	this will go to a different file at some point */ 
+this will go to a different file at some point 
 type Identity = hdk::holochain_core_types::agent::AgentId;
 
 /// Trait that provides an interface for creating and maintaining a social graph 
@@ -68,45 +68,123 @@ trait SocialGraph {
     
     fn drop_friendship(other_agent: Identity) -> Result<(), ZomeApiError>;
 }
+*/
 
 // see https://developer.holochain.org/api/0.0.47-alpha1/hdk/ for info on using the hdk library
 
 // This is a sample zome that defines an entry type "MyEntry" that can be committed to the
 // agent's chain via the exposed function create_my_entry
 
+/* Thoughts: 
+	- Validation
+		- A friend request acceptance requires an according friend request to be present
+*/
 #[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)]
-pub struct MyEntry {
-    content: String,
+pub struct Followship{
 }
 
-pub fn handle_create_my_entry(entry: MyEntry) -> ZomeApiResult<Address> {
-    let entry = Entry::App("my_entry".into(), entry.into());
-    let address = hdk::commit_entry(&entry)?;
-    Ok(address)
+#[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)] 
+pub struct FriendshipRequest{
 }
 
-pub fn handle_get_my_entry(address: Address) -> ZomeApiResult<Option<Entry>> {
-    hdk::get_entry(&address)
+#[derive(Serialize, Deserialize, Debug, DefaultJson,Clone)] 
+pub struct Friendship{
 }
 
-fn definition() -> ValidatingEntryType {
-    entry!(
-        name: "my_entry",
-        description: "this is a same entry defintion",
-        sharing: Sharing::Public,
-        validation_package: || {
-            hdk::ValidationPackageDefinition::Entry
-        },
+pub fn handle_get_my_agent_address() -> ZomeApiResult<HashString> {
+	Ok(hdk::AGENT_ADDRESS.clone().into())
+}
 
-        validation: | _validation_data: hdk::EntryValidationData<MyEntry>| {
-            Ok(())
-        }
-    )
+pub fn handle_get_my_followers() -> ZomeApiResult<Vec<HashString>> {
+	let my_followers:Vec<HashString> = Vec::new();
+	Ok(my_followers)
+}
+
+pub fn handle_get_my_followings() -> ZomeApiResult<Vec<HashString>> {
+	let my_followings:Vec<HashString> = Vec::new();
+	Ok(my_followings)
+}
+
+pub fn handle_request_friendship(
+	receiver_address: HashString,
+) -> ZomeApiResult<u32> {
+	let sender_address = hdk::AGENT_ADDRESS.clone().into();
+	let friendship_request = FriendshipRequest {};
+	let entry = Entry::App("friendship_request".into(), friendship_request.into());
+	let entry_address = hdk::commit_entry(&entry)?;
+	hdk::link_entries(
+		&sender_address,
+		&entry_address,
+		"friendship_request_send",
+		"",
+	)?;
+	hdk::link_entries(
+		&receiver_address,
+		&entry_address,
+		"friendship_request_receive",
+		"",
+	)?;
+	Ok(4) // TODO '4' ist nonsens.
+}
+
+pub fn handle_accept_friendship_request() {
+}
+
+pub fn handle_decline_friendship_request() {
+}
+
+pub fn handle_unfollow() {
+}
+
+pub fn handle_follow() {
+}
+
+pub fn handle_get_incoming_friendship_requests() -> ZomeApiResult<Vec<FriendshipRequest>> {
+	let incoming_friendship_requests:Vec<FriendshipRequest> = Vec::new();
+	Ok(incoming_friendship_requests)
+}
+
+pub fn handle_get_outgoing_friendship_requests() -> ZomeApiResult<Vec<FriendshipRequest>> {
+	let outgoing_friendship_requests:Vec<FriendshipRequest> = Vec::new();
+	Ok(outgoing_friendship_requests)
 }
 
 define_zome! {
     entries: [
-       definition()
+		entry!(
+			name: "friendship_request",
+			description: "an entry linked from an agent entry to this agent's outgoing followships",
+			sharing: Sharing::Public, 
+			validation_package: || {
+				hdk::ValidationPackageDefinition::Entry
+			},
+			validation: | _validation_data: hdk::EntryValidationData<FriendshipRequest> | {
+				Ok(())
+				// maybe check here, that there is only one list per agent
+			},
+			links: [
+				from!(
+					"%agent_id",
+					link_type: "friendship_request_send",
+					validation_package: || {
+						hdk::ValidationPackageDefinition::Entry
+					},
+					validation: |_validation_data: hdk::LinkValidationData| {
+						Ok(())
+					}
+				),
+				from!(
+					"%agent_id",
+					link_type: "friendship_request_receive", 
+					validation_package: || {
+						hdk::ValidationPackageDefinition::Entry
+					}, 
+					validation: |_validation_data: hdk::LinkValidationData| {
+						Ok(())
+					}
+				)
+			]
+		)
     ]
 
     init: || { Ok(()) }
@@ -116,19 +194,32 @@ define_zome! {
     }
 
     functions: [
-        create_my_entry: {
-            inputs: |entry: MyEntry|,
-            outputs: |result: ZomeApiResult<Address>|,
-            handler: handle_create_my_entry
-        }
-        get_my_entry: {
-            inputs: |address: Address|,
-            outputs: |result: ZomeApiResult<Option<Entry>>|,
-            handler: handle_get_my_entry
+		get_my_entry: {
+			inputs: | |,
+			outputs: |result: ZomeApiResult<HashString>|,
+			handler: handle_get_my_agent_address
+		}
+        create_friend_request: {
+            inputs: |receiver_address: hdk::holochain_persistence_api::hash::HashString|,
+            outputs: |result: ZomeApiResult<u32>|,
+            handler: handle_request_friendship
         }
     ]
-
     traits: {
-        hc_public [create_my_entry,get_my_entry]
+        hc_public [create_my_entry, get_my_entry]
+		/*SocialGraph [ 
+			my_followers,
+			followers, 
+			nth_level_followers,
+			follow, 
+			unfollow, 
+			my_friends, 
+			friends_of, 
+			request_friendship, 
+			decline_friendship, 
+			incoming_friendship_requests, 
+			outgoing_friendship_requests, 
+			drop_friendship
+		]*/
     }
 }
